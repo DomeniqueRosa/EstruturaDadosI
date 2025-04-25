b0697635c6797a99cc7149575962e1ebbca35068
Alires D M Rosa <108585145+DomeniqueRosa@users.noreply.github.com>
2025-04-25 15:52:38 -0300

    feat: estrutura lista dupla

---
+++ lista_dupla/lista_dupla.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista_circular.h"
+
+//criar lista circular vazia
+Lista* criar_lista(){
+    Lista* lista = (Lista*) malloc(sizeof(Lista));
+    lista->fim = NULL;
+    
+    return lista;
+}
+
+// Insere um valor no inicio da lista
+void inserir_inicio(Lista* lista, int valor){
+    
+}
+// Insere valor no final da lista
+void inserir_fim(Lista* lista, int valor){
+
+   
+}
+// Exibi elementos da lista
+void exibir_lista(Lista* lista){
+    
+
+}
+// Libera memoria na lista
+void liberar_lista(Lista* lista){
+
+    
+}
---
+++ lista_dupla/lista_dupla.h
@@ -0,0 +1,24 @@
+#ifndef LISTA_DUPLA_H
+#define LISTA_DUPLA_H
+
+typedef struct No{
+    int valor;
+    struct No* anterior
+    struct No* proximo;
+}No;
+
+typedef struct Lista{
+    No* cabeca;   // Ponteiro para o primeiro nó da lista
+    No* cauda;    // Ponteiro para o último nó da lista
+    int tamanho;
+}Lista;
+
+Lista* criar_lista();
+void inserir_inicio(Lista* lista, int valor);
+void inserir_fim(Lista* lista, int valor);
+void exibir_lista(Lista* lista);
+void liberar_lista(Lista* lista);
+void exibirFrente(Lista* lista): //imprime do primeiro até o último elemento.
+void exibirTras(Lista* lista): // imprime do último até o primeiro elemento.
+
+#endif
